0ca3f085da5d71cebc6b3e98ad36e870f17914b2
Poon Nguyen <nguyenduypoon@gmail.com>
2020-10-17 18:08:12 +0700

    Change build folder name of web

--- web/next.config.js
+++ web/next.config.js
@@ -2,18 +2,21 @@ const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 const path = require('path');
 
-module.exports = withPlugins([
+module.exports = withPlugins(
   [
-    withImages({
-      exclude: path.resolve(__dirname, 'assets/svg'),
-      webpack: (config) => {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack']
-        });
+    [
+      withImages({
+        exclude: path.resolve(__dirname, 'assets/svg'),
+        webpack: (config) => {
+          config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack']
+          });
 
-        return config;
-      }
-    })
-  ]
-]);
+          return config;
+        }
+      })
+    ]
+  ],
+  { distDir: 'dist' }
+);
