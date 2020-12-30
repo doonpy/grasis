eab8f2645f3ce060a939fb2511ade449e5b3c80b
Poon Nguyen <nguyenduypoon@gmail.com>
2020-12-30 16:20:09 +0700

    Refactor API server address config

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,18 @@ const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 const path = require('path');
 
+function getApiServer() {
+  if (process.env.NODE_ENV !== 'production') {
+    return process.env.API_SERVER_LOCAL;
+  }
+
+  if (process.env.IS_STAGING === '1') {
+    return process.env.API_SERVER_STAGING;
+  }
+
+  return process.env.API_SERVER_PRODUCTION;
+}
+
 module.exports = withPlugins(
   [
     [
@@ -21,7 +33,8 @@ module.exports = withPlugins(
   {
     distDir: 'dist',
     env: {
-      NEXT_PUBLIC_HEROKU_PR_NUMBER: process.env.HEROKU_PR_NUMBER
+      NEXT_PUBLIC_HEROKU_PR_NUMBER: process.env.HEROKU_PR_NUMBER,
+      NEXT_PUBLIC_API_SERVER: getApiServer()
     }
   }
 );
