ae97c6163112d250006e9774fea3a3f538c913f7
Poon Nguyen <nguyenduypoon@gmail.com>
2021-01-03 16:44:35 +0700

    Fix CI

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -23,6 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    'simple-import-sort/sort': 'error'
+    'simple-import-sort/imports': 'error'
   }
 };
